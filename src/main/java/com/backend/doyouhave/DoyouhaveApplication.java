90b64bdae9e1d56eff1b9149f88cbc7c10302ea8
Administrator <leedero01@naver.com>
2023-01-31 19:00:18 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/backend/doyouhave/DoyouhaveApplication.java
@@ -0,0 +1,13 @@
+package com.backend.doyouhave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoyouhaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoyouhaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/doyouhave/DoyouhaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.doyouhave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoyouhaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
