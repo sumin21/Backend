dcb97fce6ff22380884fc38d9d4e7edc9a8b6a7c
Eungyu-code <Agyu625@gmail.com>
2023-02-02 00:44:54 +0900

    feat: CommentController class 추가

---
+++ src/main/java/com/backend/doyouhave/controller/CommentController.java
@@ -0,0 +1,12 @@
+package com.backend.doyouhave.controller;
+
+import com.backend.doyouhave.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+}
