c93debdc22cdaa3eddae34bcecfc621087f6ae2f
Eungyu-code <Agyu625@gmail.com>
2023-02-05 22:07:08 +0900

    feat: Comment, CommentRequestDto, CommentResponseDto, MyInfoCommentResponseDto class에서의 변경점 메소드에 추가
    1. 생성 메소드 원 댓글, 대댓글에 따라 다르게 생성하도록 메소드 분리
    2. findByUser 메소드의 반환타입 Page<MyInfoCommentResponseDto>로 변경
    3. findByPost 메소드에서 newComment 생성 시 원 댓글, 대댓글인지에 따라 생성 메소드를 다르게 생성
    4. delete 메소드 추가
     - delete 메소드 수행 시 해당 객체의 isRemoved 변수가 true 값을 갖는다

--- src/main/java/com/backend/doyouhave/service/CommentService.java
+++ src/main/java/com/backend/doyouhave/service/CommentService.java
@@ -3,6 +3,7 @@ package com.backend.doyouhave.service;
 import com.backend.doyouhave.domain.comment.Comment;
 import com.backend.doyouhave.domain.comment.dto.CommentRequestDto;
 import com.backend.doyouhave.domain.comment.dto.CommentResponseDto;
+import com.backend.doyouhave.domain.comment.dto.MyInfoCommentResponseDto;
 import com.backend.doyouhave.domain.post.Post;
 import com.backend.doyouhave.domain.user.User;
 import com.backend.doyouhave.repository.comment.CommentRepository;
@@ -24,52 +25,74 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
     /*
-     * 댓글 생성
+     * 원 댓글 생성
      */
-    public Long save(CommentRequestDto commentRequestDto) {
+    public Long saveParent(CommentRequestDto commentRequestDto) {
 
-        return commentRepository.save(commentRequestDto.toEntity()).getId();
+        return commentRepository.save(commentRequestDto.toEntityParent()).getId();
+    }
+
+    /*
+     * 대댓글 생성
+     */
+    public Long saveChild(CommentRequestDto commentRequestDto) {
+
+        return commentRepository.save(commentRequestDto.toEntityChild()).getId();
     }
 
     /*
      * 댓글 수정
      */
-    public Long update(Long commentId, CommentRequestDto commentRequestDto) {
+    public void update(Long commentId, CommentRequestDto commentRequestDto) {
         // 수정하려는 댓글이 없을 경우 IllegalArgumentException 발생
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다. id=" + commentId));
 
         comment.update(commentRequestDto.getContent());
-
-        return commentId;
+        commentRepository.save(comment);
     }
 
     /*
      * 사용자가 작성한 댓글
      */
-    public Page<CommentResponseDto> findByUser(User user, Pageable pageable) {
+    public Page<MyInfoCommentResponseDto> findByUser(User user, Pageable pageable) {
 
-        return commentRepository.findByUser(user, pageable).map(CommentResponseDto::new);
+        return commentRepository.findByUser(user, pageable).map(MyInfoCommentResponseDto::new);
     }
 
     /*
      * 전단지에 작성된 댓글
      */
     public Page<CommentResponseDto> findByPost(Post post, Pageable pageable, User writer, User user) {
-        List<CommentResponseDto> comments = new ArrayList<>();
+        List<CommentResponseDto> commentDtos = new ArrayList<>();
 
         for (Comment comment : commentRepository.findByPost(post)) {
             if (comment.getUser() == writer || comment.getUser() == user) {
-                // 전단지 작성자, 혹은 댓글 작성자일 경우 실제 댓글 내용 조회 가능
-                comments.add(new CommentResponseDto(comment));
+                // 전단지 작성자, 혹은 댓글 작성자일 경우 실제 댓글 내용 조회 가능가
+                commentDtos.add(new CommentResponseDto(comment));
             } else {
                 // 전단지 작성자, 댓글 작성자 둘 다 아니면 실제 댓글 내용이 아닌 "비밀 댓글입니다."로 조회되도록 함
                 Comment newComment = new Comment();
-                newComment.create(comment.getUser(), "비밀 댓글입니다.");
-                comments.add(new CommentResponseDto(newComment));
+                if (comment.getParent() == null) {
+                    newComment.createParent(comment.getUser(), post, "비밀댓글입니다.");
+                } else {
+                    newComment.createChild(comment.getUser(), post, "비밀댓글입니다.", comment.getParent());
+                }
+                commentDtos.add(new CommentResponseDto(newComment));
             }
         }
 
-        return new PageImpl<>(comments, pageable, comments.size());
+        return new PageImpl<>(commentDtos, pageable, commentDtos.size());
+    }
+
+    /*
+     * 댓글 삭제
+     * DB에서 데이터가 삭제되지 않고 isRemoved 변수만 true 값을 갖도록 한다.
+     */
+    public void delete(Long commentId) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다. id=" + commentId));
+        comment.delete();
+        commentRepository.save(comment);
     }
 }
