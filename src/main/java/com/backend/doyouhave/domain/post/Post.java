19a084fc4a1256d2646b768a74b0d2a81980c57a
Administrator <leedero01@naver.com>
2023-02-06 06:33:22 +0900

    fix: 도메인 연관관계 및 필드 수정

--- src/main/java/com/backend/doyouhave/domain/post/Post.java
+++ src/main/java/com/backend/doyouhave/domain/post/Post.java
@@ -2,6 +2,7 @@ package com.backend.doyouhave.domain.post;
 
 import com.backend.doyouhave.domain.BaseTimeEntity;
 import com.backend.doyouhave.domain.comment.Comment;
+import com.backend.doyouhave.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Embeddable
@@ -33,18 +35,21 @@ public class Post extends BaseTimeEntity {
     @Column(nullable = false)
     private String category;
 
-    @ElementCollection
-    private List<String> tags = new ArrayList<>();
+    private String tags;
 
     @Column(nullable = false)
     private String img;
 
     private String imgSecond;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
     private List<Comment> commentList = new ArrayList<>();
 
-    public void create(String title, String content, String contactWay, String category, List<String> tags, String img, String imgSecond) {
+    public void create(String title, String content, String contactWay, String category, String tags, String img, String imgSecond) {
         this.title = title;
         this.content = content;
         this.contactWay = contactWay;
@@ -52,5 +57,10 @@ public class Post extends BaseTimeEntity {
         this.tags = tags;
         this.img = img;
         this.imgSecond = imgSecond;
+    }
+
+    public void setUser(User user) {
+        user.getPosts().add(this);
+        this.user = user;
     }
  }
