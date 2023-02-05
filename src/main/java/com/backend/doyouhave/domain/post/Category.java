78968d4287cc9d5fc345f16e86543015a3e1d744
Administrator <leedero01@naver.com>
2023-02-06 06:26:53 +0900

    feat: 태그 노출용 카테고리 추가

---
+++ src/main/java/com/backend/doyouhave/domain/post/Category.java
@@ -0,0 +1,37 @@
+package com.backend.doyouhave.domain.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String keyword;
+
+    @Column(nullable = false)
+    private int count;
+
+    @ElementCollection
+    private List<String> tags = new ArrayList<>();
+
+    @Builder
+    public Category(String keyword, int count, List<String> tags) {
+        this.keyword = keyword;
+        this.count = count;
+        this.tags = tags;
+    }
+}
