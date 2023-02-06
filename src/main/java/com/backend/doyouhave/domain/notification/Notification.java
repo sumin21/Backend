df64669fb6e37238569b88745722963121747f12
Eungyu-code <Agyu625@gmail.com>
2023-02-06 20:17:22 +0900

    feat: 기본 생성자에 대한 access 범위를 public으로 변경

--- src/main/java/com/backend/doyouhave/domain/notification/Notification.java
+++ src/main/java/com/backend/doyouhave/domain/notification/Notification.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @Entity
 public class Notification{
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
