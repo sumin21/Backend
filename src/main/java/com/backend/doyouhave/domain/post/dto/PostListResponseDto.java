a932ce2d7c0824274ac7622860f2c49c10a29398
Administrator <leedero01@naver.com>
2023-02-16 20:48:23 +0900

    fix: 전단지 골목 페이지 DTO 북마크 여부 제거

--- src/main/java/com/backend/doyouhave/domain/post/dto/PostListResponseDto.java
+++ src/main/java/com/backend/doyouhave/domain/post/dto/PostListResponseDto.java
@@ -4,12 +4,14 @@ import com.backend.doyouhave.domain.post.Post;
 import io.swagger.annotations.ApiParam;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Setter
 @Getter
 @NoArgsConstructor
 public class PostListResponseDto {
@@ -19,14 +21,11 @@ public class PostListResponseDto {
     private String categoryKeyword;
     @ApiParam(value = "글 태그", example = "['MVC', 'SECURITY', 'MYSQL']")
     private List<String> tags;
-    @ApiParam(value = "북마크한 회원 ID")
-    private List<Long> marks = new ArrayList<>();
 
     public PostListResponseDto(Post entity) {
         this.title = entity.getTitle();
         this.categoryKeyword = entity.getCategory();
         List<String> entityTags = Arrays.stream(entity.getTags().split(",")).toList();
         this.tags = entityTags;
-        this.marks = entity.getUserLikes().stream().map(user -> user.getId()).collect(Collectors.toList());
     }
 }
