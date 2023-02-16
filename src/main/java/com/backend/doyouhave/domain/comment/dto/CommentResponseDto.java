38dec66f384ff54364140168fb3263bddb73ae33
Eungyu-code <Agyu625@gmail.com>
2023-02-16 18:22:21 +0900

    feat: swagger에 필요한 annotation 추가
    DTO 변수들의 description 추가

--- src/main/java/com/backend/doyouhave/domain/comment/dto/CommentResponseDto.java
+++ src/main/java/com/backend/doyouhave/domain/comment/dto/CommentResponseDto.java
@@ -1,38 +1,49 @@
 package com.backend.doyouhave.domain.comment.dto;
 
 import com.backend.doyouhave.domain.comment.Comment;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "CommentResponseDTO")
+@ApiModel(description = "CommentResponseDTO")
 public class CommentResponseDto {
 
-    @ApiParam(value = "댓글 아이디", required = true, example = "1")
+    @ApiModelProperty(value = "댓글 아이디", required = true, example = "1")
+    @Schema(description = "댓글 아이디")
     private final Long commentId;
 
-    @ApiParam(value = "댓글 내용", required = true, example = "댓글 내용 예시")
+    @ApiModelProperty(value = "댓글 내용", required = true, example = "댓글 내용 예시")
+    @Schema(description = "댓글 내용")
     private final String content;
 
-    @ApiParam(value = "부모 댓글 아이디", required = true, example = "1(원 댓글일 시 commentId와 같고, 대댓글일 시 다르다)")
+    @ApiModelProperty(value = "부모 댓글 아이디", required = true, example = "1(원 댓글일 시 commentId와 같고, 대댓글일 시 다르다)")
+    @Schema(description = "부모 댓글 아이디")
     private final Long parentId;
 
-    @ApiParam(value = "삭제되었는지 여부", required = true, example = "true")
+    @ApiModelProperty(value = "삭제되었는지 여부", required = true, example = "true")
+    @Schema(description = "삭제 여부", required = true)
     private final boolean isRemoved;
 
-    @ApiParam(value = "비밀댓글인지 여부", required = true, example = "true")
+    @ApiModelProperty(value = "비밀댓글인지 여부", required = true, example = "true")
+    @Schema(description = "비밀댓글 여부", required = true)
     private final boolean isSecret;
 
-    @ApiParam(value = "댓글 작성자 아이디", required = true, example = "1")
+    @ApiModelProperty(value = "댓글 작성자 아이디", required = true, example = "1")
+    @Schema(description = "댓글 작성자 아이디", required = true)
     private final Long writerId;
 
-    @ApiParam(value = "전단지 작성자 아이디", required = true, example = "1")
+    @ApiModelProperty(value = "전단지 작성자 아이디", required = true, example = "1")
+    @Schema(description = "전단지 작성자 아이디", required = true)
     private final Long userId;
 
     public CommentResponseDto(Comment comment) {
         this.commentId = comment.getId();
         this.content = comment.getContent();
         // 원 댓글일 경우에는 parentId가 자기 Id
-        this.parentId = comment.getParent() == null ? comment.getId() : comment.getParent().getId();
+        this.parentId = comment.getParentId();
         this.isRemoved = comment.isRemoved();
         this.isSecret = comment.isSecret();
         this.writerId = comment.getPost().getId();
