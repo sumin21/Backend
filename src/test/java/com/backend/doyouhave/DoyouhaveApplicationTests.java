e28a368ea8b5f5c9f7940d43781780b3df14529c
Dero Lee <leedero01@naver.com>
2023-02-26 22:47:50 +0900

    fix: @SpringBootTest 주석 처리

--- src/test/java/com/backend/doyouhave/DoyouhaveApplicationTests.java
+++ src/test/java/com/backend/doyouhave/DoyouhaveApplicationTests.java
@@ -3,7 +3,7 @@ package com.backend.doyouhave;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class DoyouhaveApplicationTests {
 
 	@Test
