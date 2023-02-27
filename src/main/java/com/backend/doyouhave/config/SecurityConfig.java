0382d006ea0f4f1ae4a17fe656025eb473bd2113
Eungyu-code <Agyu625@gmail.com>
2023-02-27 16:14:28 +0900

    feat: 리눅스 서버 연결 확인을 위한 설정 추가

--- src/main/java/com/backend/doyouhave/config/SecurityConfig.java
+++ src/main/java/com/backend/doyouhave/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer configure() {
         return (web) -> web.ignoring().mvcMatchers(
+                "/",
                 "/v3/api-docs/**",
                 "/swagger-ui/**",
                 "/swagger-resources/**",
---
+++ src/main/java/com/backend/doyouhave/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.backend.doyouhave.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "hello";
+    }
+}
