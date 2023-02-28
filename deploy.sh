a81de48e744ecb40f0192766188a2775bf8aa7b4
sumin21 <tnals1178@naver.com>
2023-03-01 00:53:56 +0900

    배포 후 로그 출력 이슈

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ PROJECT_NAME=Backend
 
 cd $REPOSITORY/$PROJECT_NAME/
 
-echo "> nohup.out ???"
+echo "> nohup.out 초기화"
 
 cp /dev/null ~/doyouhave/nohup.out
 
@@ -13,33 +13,33 @@ echo "> Git Pull"
 
 git pull
 
-echo "> ???? Build ??"
+echo "> 프로젝트 Build 시작"
 
 ./gradlew build
 
-echo "> REPOSITORY ????? ??"
+echo "> REPOSITORY 디렉토리로 이동"
 
 cd $REPOSITORY
 
-echo "> Build ?? ??"
+echo "> Build 파일 복사"
 
 cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
 
-echo "?? ???? ?????? pid ??"
+echo "현재 구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
 
-echo "?? ?? ?? ?????? pid : $CURRENT_PID"
+echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-        echo "> ?? ?? ?? ??????? ???? ???? ????."
+        echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
         echo "> kill -15 $CURRENT_PID"
         kill -15 $CURRENT_PID
         sleep 5
 fi
 
-echo "> ? ?????? ??"
+echo "> 새 애플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 
@@ -47,4 +47,4 @@ echo "> JAR NAME : $JAR_NAME"
 
 nohup java -jar \
         -Dspring.config.location=classpath:/application.yml,classpath:/application.properties,/root/doyouhave/env.properties,/root/doyouhave/Backend/src/main/resources/env.properties \
-        $REPOSITORY/$JAR_NAME 2>&1 &
+        $REPOSITORY/$JAR_NAME > /dev/null 2>&1 &
